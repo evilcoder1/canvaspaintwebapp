ec407ed1bba854b4d48a5c2131334f4602194a12
evilcoder1 <128683038+evilcoder1@users.noreply.github.com>
2023-05-31 13:38:23 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -9,6 +9,9 @@ width_of_line = 1;
 canvas.addEventListener("mousedown", my_mousedown );
 
 function my_mousedown(e){
+    color = document.getElementById("color").value;
+    width_of_line = document.getElementById("width_of_line").value;
+
 mouseEvent = "mouseDown";
 }
 
@@ -48,4 +51,6 @@ canvas.addEventListener("mouseleave", my_mouseleave);
 function  my_mouseleave(e){
     mouseEvent = "mouseLeave";
 }
- 
+ function clearArea(){
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+ }
